fd218e8c159c95e23b1e14493d74558407ffe728
Cody Polland <cody.polland@gmail.com>
2022-04-30 16:22:20 -0700

    Schema and Seeds set up

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department(department_name)
+VALUES ("Managment"), 
+("Human Resources"),
+('Accounting'),
+("Marketing"),
+("Sales");
+
+INSERT INTO roll (title,salary,department_id)
+VALUES ("CEO",125000,1) , 
+("Manager",90000,1), 
+("HR Head",90000,2), 
+("Accountant", 100000, 3), 
+("Marketing", 110000, 4), 
+("Sales agent", 75000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Justin","Sparks",1,null) , 
+("Frank","Jones",1,1), 
+("Mary","Long",2,1) , 
+("Sarah","Johnson",3,1) , 
+("Karen","Mathews",4,1) , 
+("Ryan","Holliday",5,1);
+
